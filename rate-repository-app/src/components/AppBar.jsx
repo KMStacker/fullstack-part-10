2701a6cce986d4ed0837bc98ae2fa2a3e4ea24d3
km <kmannisto@hotmail.com>
2026-07-04 00:23:27 +0300

    10.7

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
 import AppBarTab from './AppBarTab';
@@ -8,25 +8,23 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.colors.appBarBackground,
+  },
+  scrollView: {
     flexDirection: 'row',
-  },
-  tab: {
-    padding: 15,
-  },
-  text: {
-    color: '#ffffff',
   },
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Link to="/">
-        <AppBarTab text="Repositories" />
-      </Link>
-      <Link to="/signin">
-        <AppBarTab text="Sign in" />
-      </Link>
+      <ScrollView horizontal style={styles.scrollView}>
+        <Link to="/">
+          <AppBarTab text="Repositories" />
+        </Link>
+        <Link to="/signin">
+          <AppBarTab text="Sign in" />
+        </Link>
+      </ScrollView>
     </View>
   );
 };
