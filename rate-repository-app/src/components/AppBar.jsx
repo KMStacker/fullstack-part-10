b2f6c0ce9b7f3d6cc1bee6de6ad587ef657e8266
km <kmannisto@hotmail.com>
2026-07-04 17:51:49 +0300

    10.16

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,8 +1,11 @@
-import { View, StyleSheet, ScrollView } from 'react-native';
+import { View, StyleSheet, ScrollView, Pressable } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
 import AppBarTab from './AppBarTab';
 import { Link } from 'react-router-native';
+import { useQuery, useApolloClient } from '@apollo/client/react';
+import { ME } from '../graphql/queries';
+import useAuthStorage from '../hooks/useAuthStorage';
 
 const styles = StyleSheet.create({
   container: {
@@ -15,15 +18,34 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
+  const { data } = useQuery(ME, {
+    fetchPolicy: 'cache-and-network',
+  });
+
+  const me = data ? data.me : null;
+
+  const signOut = async () => {
+    await authStorage.removeAccessToken();
+    await apolloClient.resetStore();
+  };
+
   return (
     <View style={styles.container}>
       <ScrollView horizontal style={styles.scrollView}>
         <Link to="/">
           <AppBarTab text="Repositories" />
         </Link>
-        <Link to="/signin">
-          <AppBarTab text="Sign in" />
-        </Link>
+        {me ? (
+          <Pressable onPress={signOut}>
+            <AppBarTab text="Sign out" />
+          </Pressable>
+        ) : (
+          <Link to="/signin">
+            <AppBarTab text="Sign in" />
+          </Link>
+        )}
       </ScrollView>
     </View>
   );
