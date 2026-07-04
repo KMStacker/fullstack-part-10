b72ffca11c197af75a41741fbcfbd9c73523c066
km <kmannisto@hotmail.com>
2026-07-04 16:36:05 +0300

    10.11

---
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -0,0 +1,18 @@
+import { useQuery } from '@apollo/client/react';
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+  const { data, error, loading, refetch } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  });
+
+  if (error) {
+    console.log('GraphQL Error:', error);
+  }
+  
+  const repositories = data ? data.repositories : undefined;
+
+  return { repositories, loading, refetch };
+};
+
+export default useRepositories;
